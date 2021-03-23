e65e9542d4bec9c7f9059897cc93b0a6b75640fb
Cosmin Vancea <73666215+cvancea@users.noreply.github.com>
2021-03-23 23:29:38 +0200

    Checker will now use venv python

--- run_tests.sh
+++ run_tests.sh
@@ -3,10 +3,13 @@
 SRC=tema
 TESTS=tests
 OUT=out
-PYTHON_CMD=python3
+PYTHON_CMD=python
 
 # Cleanup the previous run's temporary files
 rm -f ${TESTS}/*.out.sorted
+
+# Set virtual env
+source venv/Scripts/activate
 
 # Run tests
 for i in {1..9}
@@ -27,4 +30,4 @@ ${PYTHON_CMD} check_test.py 10 "${TESTS}/10.out" "${TESTS}/10.ref.out"
 
 # Pylint checks - the pylintrc file being in the same directory
 # Uncoment the following line to check your implementation's code style :)
-# ${PYTHON_CMD} -m pylint ${SRC}/*.py
+${PYTHON_CMD} -m pylint ${SRC}/*.py
