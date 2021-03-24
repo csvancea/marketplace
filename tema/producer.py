d110014c43bcf30a60ac3eb2c8ad6e26091de915
Cosmin Vancea <73666215+cvancea@users.noreply.github.com>
2021-03-24 14:04:53 +0200

    Keep adding new products forever

--- tema/producer.py
+++ tema/producer.py
@@ -41,13 +41,14 @@ class Producer(Thread):
         self._republish_wait_time = republish_wait_time
 
     def run(self):
-        for product in self._products:
-            product_type = product[0]
-            product_qty = product[1]
-            product_wait = product[2]
+        while True:
+            for product in self._products:
+                product_type = product[0]
+                product_qty = product[1]
+                product_wait = product[2]
 
-            for _ in range(product_qty):
-                while not self._marketplace.publish(self._producer_id, product_type):
-                    time.sleep(self._republish_wait_time)
+                for _ in range(product_qty):
+                    while not self._marketplace.publish(self._producer_id, product_type):
+                        time.sleep(self._republish_wait_time)
 
-                time.sleep(product_wait)
+                    time.sleep(product_wait)
